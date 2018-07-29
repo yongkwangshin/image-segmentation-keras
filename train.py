3bb98c7498039096566a4773161a7a3a83f7dd3c
Younghan Kim <yhkim2778@gmail.com>
2018-07-29 14:37:22 +0900

    sparse_categorical_crossentropy

--- train.py
+++ train.py
@@ -49,7 +49,7 @@ modelFns = { 'vgg_segnet':Models.VGGSegnet.VGGSegnet , 'vgg_unet':Models.VGGUnet
 modelFN = modelFns[ model_name ]
 
 m = modelFN( n_classes , input_height=input_height, input_width=input_width   )
-m.compile(loss='categorical_crossentropy',
+m.compile(loss='sparse_categorical_crossentropy',
       optimizer= optimizer_name ,
       metrics=['accuracy'])
 
