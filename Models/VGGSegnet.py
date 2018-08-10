d18a9a674e813bbc8260edc239cdf602330e4c72
yongkwangshin <sky83311@gmail.com>
2018-08-11 07:01:41 +0900

    model change to test2_F3_32

--- Models/VGGSegnet.py
+++ Models/VGGSegnet.py
@@ -62,17 +62,17 @@ def VGGSegnet( n_classes ,  input_height=416, input_width=608 , vgg_level=3):
 
 	o = ( UpSampling2D( (2,2), data_format='channels_first'))(o)
 	o = ( ZeroPadding2D( (1,1), data_format='channels_first'))(o)
-	o = ( Conv2D( 128, (3, 3), padding='valid', data_format='channels_first'))(o)
+	o = ( Conv2D( 32, (3, 3), padding='valid', data_format='channels_first'))(o)
 	o = ( BatchNormalization())(o)
 
 	o = ( UpSampling2D((2,2)  , data_format='channels_first' ) )(o)
 	o = ( ZeroPadding2D((1,1) , data_format='channels_first' ))(o)
-	o = ( Conv2D( 64 , (3, 3), padding='valid' , data_format='channels_first' ))(o)
+	o = ( Conv2D( 16 , (3, 3), padding='valid' , data_format='channels_first' ))(o)
 	o = ( BatchNormalization())(o)
 
 	o = ( UpSampling2D((2,2)  , data_format='channels_first' ))(o)
 	o = ( ZeroPadding2D((1,1)  , data_format='channels_first' ))(o)
-	o = ( Conv2D( 32 , (3, 3), padding='valid'  , data_format='channels_first' ))(o)
+	o = ( Conv2D( 8 , (3, 3), padding='valid'  , data_format='channels_first' ))(o)
 	o = ( BatchNormalization())(o)
 
 
