502299c87e28439eff7cb1a5570fda2a4f583267
yongkwangshin <sky83311@gmail.com>
2018-08-13 20:43:59 +0900

    problem test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import argparse
 import glob
+import os
 
 
 
@@ -21,18 +22,47 @@ img.sort()
 seg  = glob.glob( segs_path + "*.jpg"  ) + glob.glob( segs_path + "*.png"  ) +  glob.glob( segs_path + "*.jpeg"  )
 seg.sort()
 	
-
 n=len(img)
 m=len(seg)
+print('n',n,'m',m)
 
-for i in range(n)
-	for j in range(m)
-		if img[i] = seg[j]
-			count+=1
-	if count !=0
-		del seg[j]
-	count=0
 
-i=len(img)
-j=len(seg)
-print('i',i,'j',j)
+print(img[0])
+a=os.path.split(img[0])#[1]
+print(a)
+b=os.path.basename(img[0])
+print(b)
+a=os.path.splitext(img[0])
+print(a)
+
+
+for i in range(len(img)):
+	for j in range(len(seg)):
+		a=os.path.basename(seg[j])
+		b=os.path.basename(img[i])
+		# if a==b:
+		# 	break
+		# del seg[i]
+			
+			
+
+
+
+# n=len(img)
+# m=len(seg)
+# print('n',n,'m',m)
+# if img[0] == seg[0]:
+# 	count+=1
+# 	print ('dddddddddddddddddddddddddddddddddddd')
+
+
+
+
+
+# n=3
+# m=4
+# for i in range(n):
+# 	for j in range(m):
+# 		if i==j:
+# 			break
+# 		print('i',i,'j',j)
