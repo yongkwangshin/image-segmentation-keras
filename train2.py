a1def8c292c35655e6d7220380140cdd1ce535d6
yongkwangshin <sky83311@gmail.com>
2018-11-18 00:44:40 +0900

    modify epoch for fast train

--- train2.py
+++ train2.py
@@ -156,12 +156,12 @@ if validate:
 
 if not validate:
 	for ep in range( epochs ):
-		m.fit_generator( G , 100  , epochs=30 )
+		m.fit_generator( G , 100  , epochs=5 )
 		m.save_weights( save_weights_path + "." + str( ep ) )
 		m.save( save_weights_path + ".model." + str( ep ) )
 else:
 	for ep in range( epochs ):
-		m.fit_generator( G , 100  , validation_data=G2 , validation_steps=50 ,  epochs=30 )
+		m.fit_generator( G , 100  , validation_data=G2 , validation_steps=50 ,  epochs=5 )
 		m.save_weights( save_weights_path + "." + str( ep )  )
 		m.save( save_weights_path + ".model." + str( ep ) )
 
