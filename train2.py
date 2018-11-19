f28049d57d4eec647fa5aff9419d9ed3dc79d241
yongkwangshin <sky83311@gmail.com>
2018-11-19 17:43:53 +0900

    reduce 100 to 30

--- train2.py
+++ train2.py
@@ -156,11 +156,11 @@ if validate:
 
 if not validate:
 	for ep in range( epochs ):
-		m.fit_generator( G , 100  , epochs=5 )
+		m.fit_generator( G , 30  , epochs=5 )
 		m.save_weights( save_weights_path + "." + str( ep ) )
 		m.save( save_weights_path + ".model." + str( ep ) )
 else:
 	for ep in range( epochs ):
-		m.fit_generator( G , 100  , validation_data=G2 , validation_steps=50 ,  epochs=5 )
+		m.fit_generator( G , 30  , validation_data=G2 , validation_steps=50 ,  epochs=5 )
 		#m.save_weights( save_weights_path + "." + str( ep )  )
 		m.save( save_weights_path + ".model." + str( ep ) )
